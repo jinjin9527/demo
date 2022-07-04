e0845867b27d0502771cd131a88cd466ff9bc0ac
sylinx_zj <sylinx_zj@163.com>
2022-07-05 08:22:30 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @GetMapping("/helloworld")
+    public String helloWorld(){
+        return "Hello World!123456";
+    }
+}
